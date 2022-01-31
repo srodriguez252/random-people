1454da061894fe025b73ff21d8e319a0088ba0e7
Sebastian <rodriguezse13@gmail.com>
2022-01-30 20:26:19 -0500

    buttom and details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  
+  const [people , setPeople] = useState(null);
+  const apiURL = 'https://randomuser.me/api/';
+
+  function fetchPeople(){
+    fetch(apiURL)
+    .then(resp => resp.json())
+    .then(data =>  {console.log(data); setPeople(data);})
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Random People</h1>
+        <button className="fetch-button" onClick={fetchPeople}>Show Person</button>
+
+        {people && people.results.map((person , index) => {
+
+        //const fullname = JSON.stringify(person.name);
+        const fullname = person.name.title + ' ' + person.name.first + ' ' +person.name.last;
+
+        return(
+        
+        <div className="person-details" key={index}>
+            <p>{person.gender}</p>
+            <p>{fullname}</p>
+            <p></p>
+        </div>
+        )}
+        )}
+
     </div>
   );
 }
